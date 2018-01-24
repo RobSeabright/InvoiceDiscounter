ba3a85035d0bf50052bcec0761d1adfae6dd872e
RobSeabright <robseabright@gmail.com>
2018-01-24 16:29:30 -0330

    Completed Java Code

--- app/src/main/java/com/example/rob/invoicediscounter/MainActivity.java
+++ app/src/main/java/com/example/rob/invoicediscounter/MainActivity.java
@@ -9,18 +9,22 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int discountPercent = 0;
-    private double discountAmount = 0;
+
+    private EditText subtotalView;
+    private TextView percentAmountView;
+    private TextView discountAmountView;
+    private TextView totalView;
+    private double discountPercent = 0.00;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText subtotal = findViewById(R.id.subtotal);
-        final TextView percentAmt = findViewById(R.id.percentAmt);
-        final TextView discountAmt = findViewById(R.id.discountAmt);
-        final TextView total = findViewById(R.id.total);
+        subtotalView = findViewById(R.id.subtotal);
+        percentAmountView = findViewById(R.id.percentAmt);
+        discountAmountView = findViewById(R.id.discountAmt);
+        totalView = findViewById(R.id.total);
         Button plusButton = findViewById(R.id.plusButton);
         Button minusButton = findViewById(R.id.minusButtion);
 
@@ -30,10 +34,7 @@ public class MainActivity extends AppCompatActivity {
                 if (discountPercent < 100) {
                     discountPercent++;
 
-                    percentAmt.setText(discountPercent);
-                    discountAmount = Integer.parseInt(subtotal.getText().toString()) * (discountPercent / 100);
-                    discountAmt.setText(String.valueOf(discountAmount));
-                    total.setText(String.valueOf(Integer.parseInt(subtotal.getText().toString()) - discountAmount));
+                    performMaths();
                 }
             }
         });
@@ -44,15 +45,22 @@ public class MainActivity extends AppCompatActivity {
                 if (discountPercent > 0) {
                     discountPercent--;
 
-                    //TODO Can I put this code into a function so it's not repeated?
-                    percentAmt.setText(discountPercent);
-                    discountAmount = Integer.parseInt(subtotal.getText().toString()) * (discountPercent / 100);
-                    discountAmt.setText(String.valueOf(discountAmount));
-                    total.setText(String.valueOf(Integer.parseInt(subtotal.getText().toString()) - discountAmount));
+                    performMaths();
                 }
             }
         });
-
     }
+
+    private void performMaths() {
+        percentAmountView.setText(String.valueOf(discountPercent));
+
+        String discountAmountText = subtotalView.getText().toString();
+
+        double discountAmount = Double.parseDouble(discountAmountText);
+        discountAmount *= (discountPercent / 100);
+        discountAmountView.setText(String.valueOf(Math.round(discountAmount * 100.0) / 100.0));
+        totalView.setText(String.valueOf(Double.parseDouble(subtotalView.getText().toString()) - discountAmount));
+    }
+
 }
 
