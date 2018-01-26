348eead1d3a6c685bb8329154722f59402e8acad
RobSeabright <robseabright@gmail.com>
2018-01-26 13:31:36 -0330

    Changed to Linear Layout. Added user input restrictions.

--- app/src/main/java/com/example/rob/invoicediscounter/MainActivity.java
+++ app/src/main/java/com/example/rob/invoicediscounter/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.rob.invoicediscounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.text.method.DigitsKeyListener;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Get UI objects
         subtotalView = findViewById(R.id.subtotal);
         percentAmountView = findViewById(R.id.percentAmt);
         discountAmountView = findViewById(R.id.discountAmt);
@@ -30,6 +34,40 @@ public class MainActivity extends AppCompatActivity {
         Button plusButton = findViewById(R.id.plusButton);
         Button minusButton = findViewById(R.id.minusButtion);
 
+        //Limits user input to allow for only two decimal places and removes the ability to input leading zero's
+        subtotalView.setFilters(new InputFilter[] {
+                new DigitsKeyListener(Boolean.FALSE, Boolean.TRUE) {
+                    int beforeDecimal = 5, afterDecimal = 2;
+
+                    @Override
+                    public CharSequence filter(CharSequence source, int start, int end,
+                                               Spanned dest, int dstart, int dend) {
+                        String temp = subtotalView.getText() + source.toString();
+
+                        if (temp.equals(".")) {
+                            return "0.";
+                        }
+                        else if (temp.equals("0")) {
+                            return "";
+                        }
+                        else if (!temp.contains(".")) {
+                            // no decimal point placed yet
+                            if (temp.length() > beforeDecimal) {
+                                return "";
+                            }
+                        } else {
+                            temp = temp.substring(temp.indexOf(".") + 1);
+                            if (temp.length() > afterDecimal) {
+                                return "";
+                            }
+                        }
+
+                        return super.filter(source, start, end, dest, dstart, dend);
+                    }
+                }
+        });
+
+        //Performs actions on button click
         plusButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,6 +79,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Performs actions on button click
         minusButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,6 +92,9 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * Calculates and displays the amount the invoice was discounted and the user's new total.
+     */
     private void performMaths() {
         NumberFormat currency = NumberFormat.getCurrencyInstance();
         String discountAmountText = subtotalView.getText().toString();
